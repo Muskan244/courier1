77b1d3105590ed137c027fef9f7f632814839213
muskan raghav <muskan_raghav@example.com>
2024-06-21 18:07:10 +0530

    third

--- courier/settings.py
+++ courier/settings.py
@@ -120,7 +120,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# Example configuration for static files on PythonAnywhere
+STATIC_ROOT = '/home/muskanraghav.pythonanywhere.com/courier/static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
