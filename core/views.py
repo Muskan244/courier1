9f702fe95ea3f92e43ba5b39546c6643d74e2d32
muskan raghav <muskan_raghav@example.com>
2024-06-29 11:54:20 +0530

    data

--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Review, CustomUser, Parcel, TravelDetails
+from django.utils import timezone
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'core/home_page.html', {})
+    parcels = Parcel.objects.all()
+    travel_details = TravelDetails.objects.all()
+    return render(request, 'core/home_page.html', {'parcels': parcels, 'travel_details': travel_details})
 
 def index(request):
     return render(request, 'core/index.html', {})
